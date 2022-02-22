3aa02e78a5b1e4ec24a14dd964e915bb26736094
Rehan Hassan <rehan1579@gmail.com>
2022-02-22 17:17:11 +0500

    implement table UI

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+	selector: 'app-tab1',
+	templateUrl: 'tab1.page.html',
+	styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page
+{
+	public items = [
+		"", "", "", "",
+	];
 
-  constructor() {}
 
+
+	constructor()
+	{
+	}
 }
